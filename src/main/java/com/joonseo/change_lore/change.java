ced48ed138e8ac6562109da61820a732d3c7c635
joonleepc <leecpkn@naver.com>
2025-03-26 14:51:55 +0900

    first
    # Conflicts:
    #	src/main/java/com/joonseo/change_lore/change.java

--- src/main/java/com/joonseo/change_lore/change.java
+++ src/main/java/com/joonseo/change_lore/change.java
@@ -12,38 +12,36 @@ public class change implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
-        if(commandSender instanceof Player) {
+        if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
 
             if (strings.length == 0) {
 
-                player.sendMessage(ChatColor.BOLD + ""+ ChatColor.GREEN + "사용법 : /change <아이템이름>");
+                player.sendMessage(ChatColor.BOLD + "" + ChatColor.GREEN + "사용법 : /change <아이템이름>");
                 return true;
 
             }
 
 
-            String itemName = String.join(" ",strings);
+            String itemName = String.join(" ", strings);
             ItemStack item = player.getInventory().getItemInMainHand();
 
             if (item == null || item.getType().isAir()) {
-                player.sendMessage(ChatColor.BOLD + ""+ ChatColor.GREEN + "손이 비어있습니다.");
+                player.sendMessage(ChatColor.BOLD + "" + ChatColor.GREEN + "손이 비어있습니다.");
                 return true;
             }
 
             ItemMeta itemmeta = item.getItemMeta();
 
-            if(itemmeta != null) {
+            itemName = ChatColor.translateAlternateColorCodes('&', itemName);
 
+            itemmeta.setDisplayName(itemName);
+            item.setItemMeta(itemmeta);
 
-
-
-
-
+            player.sendMessage(ChatColor.LIGHT_PURPLE + "아이템 이름이 변경되었습니다.");
+            return true;
 
         }
-
-
         return false;
     }
 }
